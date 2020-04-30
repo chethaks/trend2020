31d8c83d465462bef9c5a09d5f6b09392f9f59f6
chethaks <chethan.kumar021@harman.com>
2020-05-01 03:03:54 +0530

    Interaction changes

--- assets/js/trend-main.js
+++ assets/js/trend-main.js
@@ -1,5 +1,6 @@
 /** Page Jquery */
 $(document).ready(function() {
+    /** Sticky Menu */
     $(window).on('scroll', function () {
         if ( $(window).scrollTop() > 10 ) {
             $('.navbar').addClass('active');
@@ -7,6 +8,33 @@ $(document).ready(function() {
             $('.navbar').removeClass('active');
         }
     });
+
+    /** Scroll to section */
+    $("#market").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#marketSection").offset().top
+        }, 2000);
+    });
+
+    $("#covid").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#covidSection").offset().top
+        }, 2000);
+    });
+
+    /** Scroll to top */
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scrollTop').fadeIn(); 
+        } else { 
+            $('#scrollTop').fadeOut(); 
+        } 
+    });
+
+    $('#scrollTop').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 2000);
+        return false; 
+    }); 
 });
 
 /** Page Parallax */
