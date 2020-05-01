b930a3ab020c9452ee6eba910b0bdbb541b6a9db
chethaks <chethan.kumar021@harman.com>
2020-05-01 16:54:17 +0530

    Latest changes

--- assets/js/trend-main.js
+++ assets/js/trend-main.js
@@ -1,41 +1,63 @@
 /** Page Jquery */
-$(document).ready(function() {
-    /** Sticky Menu */
-    $(window).on('scroll', function () {
-        if ( $(window).scrollTop() > 10 ) {
-            $('.navbar').addClass('active');
-        } else {
-            $('.navbar').removeClass('active');
-        }
-    });
+$(document).ready(function () {
+  /** Sticky Menu */
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 10) {
+      $(".navbar").addClass("active");
+    } else {
+      $(".navbar").removeClass("active");
+    }
+  });
 
-    /** Scroll to section */
-    $("#market").click(function() {
-        $('html, body').animate({
-            scrollTop: $("#marketSection").offset().top
-        }, 2000);
-    });
+  /** Scroll to section */
+  $("#market").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $("#marketSection").offset().top,
+      },
+      2000,
+    );
+  });
 
-    $("#covid").click(function() {
-        $('html, body').animate({
-            scrollTop: $("#covidSection").offset().top
-        }, 2000);
-    });
+  $("#covid").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $("#covidSection").offset().top,
+      },
+      2000,
+    );
+  });
 
-    /** Scroll to top */
-    $(window).scroll(function(){ 
-        if ($(this).scrollTop() > 100) { 
-            $('#scrollTop').fadeIn(); 
-        } else { 
-            $('#scrollTop').fadeOut(); 
-        } 
-    });
+  /** Scroll to top */
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $("#scrollTop").fadeIn();
+    } else {
+      $("#scrollTop").fadeOut();
+    }
+  });
 
-    $('#scrollTop').click(function(){ 
-        $("html, body").animate({ scrollTop: 0 }, 2000);
-        return false; 
-    }); 
+  $("#scrollTop").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 2000);
+    return false;
+  });
 });
 
 /** Page Parallax */
-var rellax = new Rellax('.rellax'); // Accepts any class name
+var rellax = new Rellax(".rellax"); // Accepts any class name
+
+/** Slide Section */
+
+$(function () {
+  $(".show-section-2").hide();
+
+  $(".prev, .next").bind("click", function () {
+    $(".show-section-1, .show-section-2").hide();
+
+    if ($(this).attr("class") == "prev") {
+      $(".show-section-1").show();
+    } else {
+      $(".show-section-2").show();
+    }
+  });
+});
