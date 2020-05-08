59a1c31a20c03ca80bb1a1fc08d6bcf3e661cce0
chethaks <chethan.kumar021@harman.com>
2020-05-08 18:05:10 +0530

    Code merge

--- assets/js/trend-main.js
+++ assets/js/trend-main.js
@@ -1,5 +1,6 @@
 /** Page Jquery */
 
+/** Sticky Menu */
 $(window).on("scroll", function () {
   if ($(window).scrollTop() > 10) {
     $(".navbar").addClass("active");
@@ -8,6 +9,136 @@ $(window).on("scroll", function () {
   }
   return false;
 });
+
+// HCS CLICK DOWN ARROW START
+$("#hcs-connected-lifestyle").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $("#connected-lifestyle").offset().top,
+    },
+    2000,
+  );
+  return false;
+});
+// HCS CLICK DOWN ARROW END
+
+// LS CLICK DOWN ARROW START
+$("#ls-cons-ptrns").click(function () {
+
+  $("html, body").animate(
+    {
+      scrollTop: $("#consumer-pttrns").offset().top,
+    },
+    2000,
+  );
+  return false;
+});
+// LS CLICK DOWN ARROW END
+
+
+// PS CLICK DOWN ARROW START
+$("#vertical-darrow").click(function () {
+
+  $("html, body").animate(
+    {
+      scrollTop: $("#verticalSection").offset().top,
+    },
+    2000,
+  );
+  return false;
+});
+// PS CLICK DOWN ARROW END
+
+
+// PRO SOLUTION - END USER
+$("#prosol-enduser").click(function (e) {
+  $("#end-user").addClass("show-section");
+  $("#vertical").removeClass("show-section");
+  $("#competitors").removeClass("show-section");
+  $("#channel").removeClass("show-section");
+  $("#product").removeClass("show-section");
+  e.preventDefault();
+  $("html, body").animate(
+    {
+      scrollTop: $("#end-user").offset().top,
+    },
+    2000,
+  );
+  return false;
+});
+
+// PRO SOLUTION - vertical
+$("#prosol-vertical").click(function (e) {
+  $("#end-user").removeClass("show-section");
+  $("#vertical").addClass("show-section");
+  $("#competitors").removeClass("show-section");
+  $("#channel").removeClass("show-section");
+  $("#product").removeClass("show-section");
+  e.preventDefault();
+  $("html, body").animate(
+    {
+      scrollTop: $("#vertical").offset().top,
+    },
+    2000,
+  );
+  return false;
+});
+
+
+
+
+// PRO SOLUTION - competitors
+$("#prosol-competitors").click(function (e) {
+  $("#end-user").removeClass("show-section");
+  $("#vertical").removeClass("show-section");
+  $("#competitors").addClass("show-section");
+  $("#channel").removeClass("show-section");
+  $("#product").removeClass("show-section");
+  e.preventDefault();
+  $("html, body").animate(
+    {
+      scrollTop: $("#competitors").offset().top,
+    },
+    2000,
+  );
+  return false;
+});
+
+
+// PRO SOLUTION - channel
+$("#prosol-channel").click(function (e) {
+  $("#end-user").removeClass("show-section");
+  $("#vertical").removeClass("show-section");
+  $("#competitors").removeClass("show-section");
+  $("#channel").addClass("show-section");
+  $("#product").removeClass("show-section");
+  e.preventDefault();
+  $("html, body").animate(
+    {
+      scrollTop: $("#channel").offset().top,
+    },
+    2000,
+  );
+  return false;
+});
+
+// PRO SOLUTION - product
+$("#prosol-product").click(function (e) {
+  $("#end-user").removeClass("show-section");
+  $("#vertical").removeClass("show-section");
+  $("#competitors").removeClass("show-section");
+  $("#channel").removeClass("show-section");
+  $("#product").addClass("show-section");
+  e.preventDefault();
+  $("html, body").animate(
+    {
+      scrollTop: $("#product").offset().top,
+    },
+    2000,
+  );
+  return false;
+});
+
 
 /** Scroll to section */
 $("#market").click(function () {
@@ -91,7 +222,7 @@ $(window).scroll(function () {
 });
 
 $("#scrollTop").click(function () {
-  $("html, body").animate({ scrollTop: 0 }, 2000);
+  $("html, body").animate({ scrollTop: 0 }, 1000);
   return false;
 });
 
@@ -108,14 +239,12 @@ window.onload = function () {
 }
 
 /** Slide Section */
-
 $("#covidClick").click(function () {
-  $("#openCovideSection").slideDown("slow");
   $("html, body").animate(
     {
       scrollTop: $("#openCovideSection").offset().top,
     },
     2000,
   );
+  return false;
 });
-
