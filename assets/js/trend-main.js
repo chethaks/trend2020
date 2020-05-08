d9a45f6b8812020df72573cc2f1aef49d26bdfa8
vijayanmharman <64908458+vijayanmharman@users.noreply.github.com>
2020-05-08 16:15:59 +0530

    Added new section in LS page
    Added new section in LS page
    Highlighted the page main menu link

--- assets/js/trend-main.js
+++ assets/js/trend-main.js
@@ -222,7 +222,7 @@ $(window).scroll(function () {
 });
 
 $("#scrollTop").click(function () {
-  $("html, body").animate({ scrollTop: 0 }, 2000);
+  $("html, body").animate({ scrollTop: 0 }, 1000);
   return false;
 });
 
