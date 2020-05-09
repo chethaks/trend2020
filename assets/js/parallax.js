b9a69012a1f1013855ecaf0100cc07c1a28240f6
chethaks <chethan.kumar021@harman.com>
2020-05-09 21:34:41 +0530

    Review comment updates

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -6,7 +6,7 @@ const video = intro.querySelector("video");
 let tl1 = new TimelineMax();
 
 let scene = new ScrollMagic.Scene({
-  duration: 2000,
+  duration: 0,
   triggerElement: intro,
   triggerHook: 0
 })
@@ -14,7 +14,7 @@ let scene = new ScrollMagic.Scene({
   .setPin(tl1)
   .addTo(controller);
 
-let accelamount = 0.1;
+let accelamount = 1;
 let scrollpos = 0;
 let delay = 0;
 
@@ -29,17 +29,17 @@ setInterval(() => {
 
 
 let tl2 = new TimelineMax()
-				.from($(".parallax1, .parallax2, .parallax3, .parallax4, .parallax5, .parallax6"), 1, {y: "-200%", autoAlpha:0})
+				.from($(".parallax1, .parallax2, .parallax3, .parallax4, .parallax5, .parallax6"), 1, {y: "100%", autoAlpha:0})
 				.to(".parallax1", 1, {y: "0%", autoAlpha:1})
-				.to(".parallax1", 1, {y: "-=50%", autoAlpha:0})
+				.to(".parallax1", 1, {y: "-=10%", autoAlpha:0})
 				.to(".parallax2", 1, {y: "0%", autoAlpha:1})
-				.to(".parallax2", 1, {y: "-=50%", autoAlpha:0})
+				.to(".parallax2", 1, {y: "-=10%", autoAlpha:0})
         .to(".parallax3", 1, {y: "0%", autoAlpha:1})
-        .to(".parallax3", 1, {y: "-=50%", autoAlpha:0})
+        .to(".parallax3", 1, {y: "-=10%", autoAlpha:0})
         .to(".parallax4", 1, {y: "0%", autoAlpha:1})
-        .to(".parallax4", 1, {y: "-=50%", autoAlpha:0})
+        .to(".parallax4", 1, {y: "-=10%", autoAlpha:0})
         .to(".parallax5", 1, {y: "0%", autoAlpha:1})
-        .to(".parallax5", 1, {y: "-=50%", autoAlpha:0})
+        .to(".parallax5", 1, {y: "-=10%", autoAlpha:0})
         .to(".parallax6", 1, {y: "0%", autoAlpha:1});
 
 
@@ -52,5 +52,5 @@ slideAnimation.add(tl1, 0).add(tl2, 0);
 new ScrollMagic.Scene({
   triggerElement: intro,
   triggerHook: "onLeave",
-  duration: 7000
+  duration: 6000
 }).setPin(intro).setTween(slideAnimation).addTo(controller);
