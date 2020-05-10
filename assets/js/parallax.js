ce3652d5d66580254049f928f5df983f19261dfe
chethaks <chethan.kumar021@harman.com>
2020-05-10 23:55:28 +0530

    CC Changes

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -52,5 +52,5 @@ slideAnimation.add(tl1, 0).add(tl2, 0);
 new ScrollMagic.Scene({
   triggerElement: intro,
   triggerHook: "onLeave",
-  duration: 6000
+  duration: 4000
 }).setPin(intro).setTween(slideAnimation).addTo(controller);
